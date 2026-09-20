d6b41d4ec53b335833840fdac7e97641b58ab3c9
Danai Nanou <danai.nanou@gmail.com>
2026-09-20 16:41:36 +0100

    Import and flesh out search component html

--- CrimeFinder-challenge/src/App.tsx
+++ CrimeFinder-challenge/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
+import { SearchComponent } from './components/SearchComponent'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <SearchComponent />
     </>
   )
 }
---
+++ CrimeFinder-challenge/src/components/SearchComponent.tsx
@@ -0,0 +1,13 @@
+export function SearchComponent() {
+    return (
+        <div className="search-form">
+            <form>
+                <input
+                    type="search"
+                    placeholder="Search"
+                />
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    )
+}
